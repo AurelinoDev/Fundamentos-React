f3fd2d2463364408031dbabb2c4d0908e75cc08a
Igor-Aurelino <i.rufino@aluno.senai.br>
2025-04-18 21:26:11 -0300

    18/04

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
-function App() {
-  const [count, setCount] = useState(0)
+import { Header } from './components/Header';
+import { Post } from './Post';
+import './global.css'
 
-  return (
-   <h1>Hello Word</h1>
+export function App(){
+  return(
+    <div>
+      <Header />
+      <Post
+        author= "Igor Aurelino"
+        content= "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit sit vel incidunt ut dolores totam, voluptatem exercitationem nam autem magnam, officiis, tenetur voluptate ad dicta illum! Necessitatibus commodi libero omnis."
+      />
+      <Post
+        author= "Melina Tomé"
+        content= "Eu te amo"
+      />
+        
+    </div>
   )
 }
-export default App
